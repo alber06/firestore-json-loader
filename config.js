059b95ae3161c17b9df4ffc2ae8c1c2a847a49c0
Alberto Fernández Gómez <afernandez@jabary.com>
2019-08-14 13:27:05 +0200

    Added json loader tool

---
+++ config.js
@@ -0,0 +1,12 @@
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyCGhsqxTeEneDyuMNZP4FTHZTLDnRHBnMk",
+  authDomain: "userbrowsertest-int.firebaseapp.com",
+  databaseURL: "https://userbrowsertest-int.firebaseio.com",
+  projectId: "userbrowsertest-int",
+  storageBucket: "userbrowsertest-int.appspot.com",
+  messagingSenderId: "460534725771",
+  appId: "1:460534725771:web:1939fe9d5a2b6468"
+};
+// Exports
+module.exports = firebaseConfig;
